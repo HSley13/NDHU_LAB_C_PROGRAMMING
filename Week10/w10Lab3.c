4931a234b2672a7feb81da8768e165fddbc6e246
Sley HORTES <sleyhortes13@gmail.com>
2023-12-30 13:19:53 +0800

    Done Preparing Final Week6 - Week10

--- Week10/w10Lab3.c
+++ Week10/w10Lab3.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct 
 {
     char integer[1000];
     char decimal[1000];
-} DeciVar;
+} deci_var;
 
-void add_deci(DeciVar a, DeciVar b, DeciVar *c) 
+void read_deci(deci_var *a)
+{
+    char input[2001];
+    scanf("%s", input);
+
+    sscanf(input, "%[^.].%s", a->integer, a->decimal);
+}
+
+void add_deci(deci_var a, deci_var b, deci_var *c)
 {
     int len_a_int = strlen(a.integer), len_b_int = strlen(b.integer);
     int len_a_dec = strlen(a.decimal), len_b_dec = strlen(b.decimal);
 
     while (len_a_int < len_b_int) a.integer[len_a_int++] = '0';
     while (len_b_int < len_a_int) b.integer[len_b_int++] = '0';
-    
+
     a.integer[len_a_int] = '\0';
     b.integer[len_b_int] = '\0';
 
@@ -26,59 +35,52 @@ void add_deci(DeciVar a, DeciVar b, DeciVar *c)
 
     int carry = 0;
     c->decimal[len_a_dec] = '\0';
-    while (len_a_dec--) 
+    while (len_a_dec--)
     {
-      int sum = (a.decimal[len_a_dec] - '0') + (b.decimal[len_a_dec] - '0') + carry;
-      carry = sum / 10;
-    	sum %= 10;
-      c->decimal[len_a_dec] = sum + '0';
+        int sum = (a.decimal[len_a_dec] - '0') + (b.decimal[len_a_dec] - '0') + carry;
+        carry = sum / 10;
+        sum %= 10;
+
+        c->decimal[len_a_dec] = sum + '0';
     }
 
     c->integer[len_a_int] = '\0';
-    while (len_a_int--) 
+    while (len_a_int--)
     {
-      int sum = (a.integer[len_a_int] - '0') + (b.integer[len_a_int] - '0') + carry;
-      carry = sum / 10;
-      sum %= 10;
-      c->integer[len_a_int] = sum  + '0';
+        int sum = (a.integer[len_a_int] - '0') + (b.integer[len_a_int] - '0') + carry;
+        carry = sum / 10;
+        sum %= 10;
+
+        c->integer[len_a_int] = sum + '0';
     }
 
-    if (carry) 
+    if (carry)
     {
         int len = strlen(c->integer);
 
-        for (int i = len; i > 0; --i) c->integer[i] = c->integer[i - 1];
-    
+        for (int i = len; i > 0; i--) c->integer[i] = c->integer[i - 1];
+
         c->integer[0] = carry + '0';
         c->integer[len + 1] = '\0';
     }
 }
 
-void read_deci(DeciVar *a) 
+void print_deci(deci_var x)
 {
-    char input[2001];
-    scanf("%s", input);
+    printf("%s", x.integer);
 
-    sscanf(input, "%[^.].%s", a->integer, a->decimal);
+    if(x.decimal[0] != '0') printf(".%s", x.decimal);
+
+    printf("\n"); 
 }
 
-void print_deci(DeciVar a) 
+int main(void)
 {
-    printf("%s", a.integer);
-
-    if (a.decimal[0] != '0') printf(".%s", a.decimal);
-    
-    printf("\n");
-}
-
-int main(void) 
-{
-    DeciVar a, b, c;
+    deci_var a, b, c;
 
     read_deci(&a);
     read_deci(&b);
 
     add_deci(a, b, &c);
-
     print_deci(c);
 }
--- Week10/w10Lab5.c
+++ Week10/w10Lab5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define ROWS 9
 #define COLS 9
@@ -7,9 +8,9 @@
 char board[ROWS][COLS];
 char visited[ROWS][COLS];
 
-int depth_first_search(int i, int j) 
+int depth_first_search(int i, int j)
 {
-    if (i < 0 || j < 0 || i >= ROWS || j >= COLS) return 0;
+    if (i < 0 || i >= ROWS || j < 0 || j >= COLS) return 0;
 
     if (visited[i][j]) return 0;
 
@@ -24,26 +25,26 @@ int depth_first_search(int i, int j)
     flag |= depth_first_search(i - 1, j);
     flag |= depth_first_search(i, j + 1);
     flag |= depth_first_search(i, j - 1);
-    
+
     return flag;
 }
 
-int main(void) 
+int main(void)
 {
-    int test_cases, i, j;
+    int test_cases;
     scanf("%d", &test_cases);
 
-    while (test_cases--) 
+    while(test_cases--)
     {
-        for (i = 0; i < ROWS; i++) scanf("%s", board[i]);
+        for (int i = 0; i < ROWS; i++) scanf("%s", board[i]);
 
         int white_count = 0, black_count = 0;
 
-        for (i = 0; i < ROWS; i++) 
+        for (int i = 0; i < ROWS; i++)
         {
-            for (j = 0; j < COLS; j++) 
+            for (int j = 0; j < COLS; j++)
             {
-                if (board[i][j] == '.') 
+                if (board[i][j] == '.')
                 {
                     memset(visited, 0, sizeof(visited));
                     int flag = depth_first_search(i, j);
@@ -51,14 +52,14 @@ int main(void)
                     if (flag == 1) white_count++;
 
                     if (flag == 2) black_count++;
-                } 
+                }
 
                 else if (board[i][j] == 'O') white_count++;
-                
+
                 else black_count++;
             }
         }
-        
+
         printf("Black %d White %d\n", black_count, white_count);
     }
 }
